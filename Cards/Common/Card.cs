89a8746413c3084c0dd79bb0056a7f30e9d15c14
Diana Schweers <dc.schweers@gmail.com>
2025-01-14 18:23:37 +0100

    Initialize sample data - oops? Settled for list of object for address field

--- Cards/Common/Card.cs
+++ Cards/Common/Card.cs
@@ -1,29 +1,25 @@
 ﻿using System;
 using WPF_Project_Logic.Cards.Common.Agency;
 
+
 namespace WPF_Project_Logic.Cards.Common.Card {
     public abstract class Card {
 
 
         // This forces any class that inherits from 'Card' to define its own 'Name'.
         public abstract string Name { get; set; }
-        public abstract int ID { get; set; }
+        public abstract int ID {get; set;}
        
         // Assign the 'name' value in the subclass when it's created.
-        protected Card(string name, int id) 
-        {
+        protected Card(string name, int id) {
 
             // Validate the name and get the validation result
             string validationMessage = Validation.ValidateName(name);
 
-            if (validationMessage != null) 
-            {
-                // If the validation fails, throw an ArgumentException with the error message
-                throw new ArgumentException(validationMessage);
-            }
 
+        // Assign the 'name' value in the subclass when it's created.
+        protected Card(string name, int iD) {
             Name = name;
-            ID = id;
         }
 
     }
